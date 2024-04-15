39bc2746109bc2dc4ca552c6afde843f720123ae
Lazim Rayan <lazimrayanfreelancer@gmail.com>
2024-04-15 16:57:51 +0600

    Input moved as component, Conditional Rendering added to verify Registered Status

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import Input from "./Input.jsx";
 
 function Form(props) {
-  return (
-    <form className="form">
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      {!props.isRegistered && (
-        <input type="password" placeholder="Confirm Password" />
-      )}
+  const { userIsRegistered } = props;
 
-      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
-    </form>
+  return (
+    <div>
+      <form className="form">
+        <Input type="text" placehold="Username" />
+        <Input type="password" placehold="Password" />
+        {userIsRegistered?null:<Input type="password" placehold="Confirm Password" />}
+        {userIsRegistered?<button type="submit">Login</button>:<button type="submit">Register</button>}
+      </form>
+    </div>
   );
 }
 
 export default Form;
+
